ffd2f59eb062c9d4e39675491a4c0d86e80506cf
munny-d <munny_d@hotmail.co.uk>
2020-02-14 12:31:07 +0000

    Updates

---
+++ precache-manifest.cfa9ad86ad97fe6416f4f821eef6cceb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16ed7a06d41e0cd18076d8667b1af6e",
+    "url": "/reactsmartbrain/index.html"
+  },
+  {
+    "revision": "79d7d3244768cf664999",
+    "url": "/reactsmartbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a553e205bebe3296d5ef",
+    "url": "/reactsmartbrain/static/css/main.f1d91f08.chunk.css"
+  },
+  {
+    "revision": "79d7d3244768cf664999",
+    "url": "/reactsmartbrain/static/js/2.c3af018b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactsmartbrain/static/js/2.c3af018b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a553e205bebe3296d5ef",
+    "url": "/reactsmartbrain/static/js/main.47645eec.chunk.js"
+  },
+  {
+    "revision": "06b946476338c578f7e3",
+    "url": "/reactsmartbrain/static/js/runtime-main.e9f38e7d.js"
+  },
+  {
+    "revision": "6a56bff78b05cd2552cad01aadd2d291",
+    "url": "/reactsmartbrain/static/media/dcg.6a56bff7.png"
+  }
+]);
